88168e95cecbfd3e07b7b0f02f0c8fe688a89b9c
My-Kairo <marciatsoho@gmail.com>
2022-03-15 15:47:21 +0200

    third commit'

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ const btnElem = document.querySelector(".btn");
 
 let words = "";
 
-let myString = ()=> {
+let myString = () => {
     let theString = textElem.value;
-    let arr = theString.split(" ,")
+    let arr = theString.split(" ")
+    let sent = ""
+    for ( let i = 0; i < arr.length; i++){
+        const array = arr[i];
+        if (array.length >= 5){
+                 
+                sent += `<mark style="color: red;">
+                 ${array}
+             </mark> `
+            }
+        else {
+            sent+= array + ' '
+        }
+
+    }
     
-    console.log(arr);
-    
-    
-    displayElem.innerHTML = arr;
+
+    console.log(sent);
+
+
+    displayElem.innerHTML = sent;
 }
 btnElem.addEventListener('click', myString)
