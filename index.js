16a62a12e86f05a7c6fff5c27d33253b1f692e13
My-Kairo <marciatsoho@gmail.com>
2022-03-16 12:51:00 +0200

    checkbox

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const displayElem = document.querySelector(".display");
 const textElem = document.querySelector(".text");
 const btnElem = document.querySelector(".btn");
-const countElem = document.querySelector(".counter")
+const countElem = document.querySelector(".counter");
+const checkElem = document.querySelector(".check")
 
 let words = "";
 
@@ -10,21 +11,21 @@ let myString = () => {
     let arr = theString.split(/\s/)
     let sent = ""
     let counter = 0;
-    for ( let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
         const array = arr[i];
-        if (array.length >= 5){
-                 
-                sent += `<mark style="color: red;">
+        if (array.length >= 5) {
+
+            sent += `<mark style="color: black;">
                  ${array}
              </mark> `
-            }
+        }
         else {
-            sent+= array + ' '
-            counter ++
+            sent += array + ' '
+            counter++
         }
 
     }
-    
+
 
     console.log(sent);
 
@@ -34,3 +35,35 @@ let myString = () => {
 }
 
 btnElem.addEventListener('click', myString)
+
+let hide = () => {
+    let theString = textElem.value;
+    let arr = theString.split(/\s/);
+    let sent = ""
+    if (checkElem.checked == true) {
+        for (let i = 0; i < arr.length; i++) {
+            const array = arr[i]
+            // displayElem.style.display = "block"
+            if (array.length >= 5) {
+                sent += `<span style="display: none">${array} </span> `
+            } else {
+                // displayElem.style.display = "none"
+                sent += array + ' '
+            }
+
+        }
+        console.log(sent);
+        displayElem.innerHTML = sent;
+
+    }
+    else {
+        myString()
+     }
+}
+checkElem.addEventListener('click', hide)
+
+// if (checkElem.checked == false){
+//     displayElem.style.display = "block";
+// }else{
+//     displayElem.style.display = "none";
+// }
