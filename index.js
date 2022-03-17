4f9dd9fef305091e3960254fa667b53bca2db7e4
My-Kairo <marciatsoho@gmail.com>
2022-03-17 11:41:21 +0200

    long word

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const displayElem = document.querySelector(".display");
 const textElem = document.querySelector(".text");
 const btnElem = document.querySelector(".btn");
 const countElem = document.querySelector(".counter");
-const checkElem = document.querySelector(".check")
+const checkElem = document.querySelector(".check");
+const longElem = document.querySelector(".long")
 
 let words = "";
 
@@ -46,6 +47,14 @@ let hide = () => {
             // displayElem.style.display = "block"
             if (array.length <= 5) {
                 sent += `<span style="display: none">${array} </span> `
+                // if (array.length > 8){
+                //     sent += `<span style ="background-color: green;">${array}</span>`
+
+                // }
+            }else if (sent >= arr.length){
+                sent += `<mark style="color: black;">
+                ${array}
+            </mark> `
             } else {
                 // displayElem.style.display = "none"
                 sent += array + ' '
@@ -61,6 +70,32 @@ let hide = () => {
      }
 }
 checkElem.addEventListener('click', hide)
+
+let theLongest = ()=> {
+    let theString = textElem.value;
+    let arr = theString.split(/\s/);
+    let long = " ";
+    if (checkElem.checked == true){
+        for (let i = 0; i < arr.length; i++){
+            let array = arr[i];
+            if(long >= array.length ){
+                long == array.length; 
+                array += `<mark style="color: black;">
+                ${array}
+            </mark> `
+            }else {
+                long += array + ' '
+            }
+        }
+        displayElem.innerHTML = long;
+    }
+    // else {
+    //     hide()
+    // }
+
+}
+
+longElem.addEventListener('click', theLongest)
 
 // if (checkElem.checked == false){
 //     displayElem.style.display = "block";
